dee2d43e445d57f3bb160a1425f8a9a6d72e963b
Arturo Espinoza Carrasco <clonjago@gmail.com>
2020-06-15 18:10:08 -0400

    Created Base html template, and made index templace inherit fromt it.

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
+from flask import render_template
 from app import app
 
-@app.route("/") # decorator
-@app.route("/index") # decorator
+@app.route('/')
+@app.route('/index')
 def index():
-    return "Hello World!"
-
-# A decorator modifies the function that follows it.
-# A common pattern with decorators is to use them to register
-# functions as callbacks for certain events. In this case,
-# the @app.route decorator creates an association between
-# the URL given as an argument and the function.
-# In this example there are two decorators, which associate
-# the URLs / and /index to this function.
-# This means that when a web browser requests either of
-# these two URLs, Flask is going to invoke this function
-# and pass the return value of it back to the browser as a
-# response.
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
