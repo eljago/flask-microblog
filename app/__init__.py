c2aa998ad3b80d666d640c48f0a99c4142b13f04
Arturo Espinoza Carrasco <clonjago@gmail.com>
2020-06-15 18:28:31 -0400

    Created a config module to store environment variables. Loading it in the app package

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 # Creates the application object as an instance of class Flask imported from the flask package
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # The bottom import is a workaround to circular imports, a common problem with Flask applications.
 from app import routes
