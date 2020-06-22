bdba1530b755660add4657dbe2adb209964830df
Arturo Espinoza Carrasco <clonjago@gmail.com>
2020-06-22 11:06:05 -0400

    Setup sql-alchemy and flask-migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # Creates the application object as an instance of class Flask imported from the flask package
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# SQLAlechemy and Flask-Migrate
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # The bottom import is a workaround to circular imports, a common problem with Flask applications.
-from app import routes
+from app import routes, models
 
